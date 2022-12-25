c1dd37cc819470df2a137990c2006535ca581f92
amrlsymlx <amirulsyamil.ahmad@gmail.com>
2022-12-26 03:07:18 +0800

    Update 2_script.js

--- 2_script.js
+++ 2_script.js
@@ -1,8 +1,8 @@
 let currentPage = 1;
-
-const DISCOVER_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page='
+const apik = '3fd2be6f0c70a2a598f084ddfb75487c';
+const DISCOVER_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key='+apik+'&page='
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key='+apik
 
 const main = document.getElementById('main')
 
@@ -52,10 +52,8 @@ function getClassByRate(vote) {
   }
 }
 
-// -------------------------------------------------
-
 // -----------------Fetch Genre List----------------
-fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=3fd2be6f0c70a2a598f084ddfb75487c&language=en-US`)
+fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=`+apik+`&language=en-US`)
 .then(response => response.json())
 .then(data => {
 	const genres = data.genres;
@@ -69,30 +67,6 @@ fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=3fd2be6f0c70a2a598f
 	});
 });
 
-// ---------------------------------------------------------------------------------
-
-// const GENRE_API = 'https://api.themoviedb.org/3/genre/movie/list?api_key=3fd2be6f0c70a2a598f084ddfb75487c&language=en-US'
-
-// getList (GENRE_API)
-
-// async function getList(url) {
-//     const res = await fetch(url)
-//     const data = await res.json()
-
-//     // const genres = data['genres'];
-//     const genres = data.genres;
-// 	const genreSelect = document.getElementById('list');
-
-// 	genres.forEach(genre => {
-// 	    const option = document.createElement('option');
-// 	    option.value = genre.id;
-// 	    option.textContent = genre.name;
-// 	    genreSelect.appendChild(option);
-// 	});
-// }
-
-
-// ---------------------------------------------------
 
 // ------------------Increment Button for Initial List------------------
 
@@ -109,21 +83,26 @@ const updatePage = (newPage) => {
 incrementButton.addEventListener('click', () => updatePage(currentPage + 1));
 decrementButton.addEventListener('click', () => updatePage(currentPage - 1));
 
-
-// --------------------------------------------------
-
 // ------------------------------------------------
 
-const searchInput = document.querySelector('#search')
-const genreSelect = document.querySelector('#list');
+const goButton = document.getElementById('go-button')  
 
-const dispInput = () => {
+// const genreOption = genreSelect.querySelector(`option[value="${genre}"]`);
+// const genreID = genreOption.value
+// const query = searchInput.value
+
+const showSearch = () => {
+  const searchInput = document.querySelector('#search')
+  const genreSelect = document.querySelector('#list')
   const genre = genreSelect.value;
   const genreOption = genreSelect.querySelector(`option[value="${genre}"]`);
-  const title = searchInput.value
+  const genreID = genreOption.value
+  const query = searchInput.value
+  console.log(genreID)
+  console.log(query)
+
+  getMovies(SEARCH_API+ '&query='+ query+ '&with_genres='+ genreID)
   
-  console.log(genreOption.value)
-  console.log(title)
 }
 
-searchInput.addEventListener('input', dispInput);
+goButton.addEventListener('click', showSearch)
